f646e88f7030ee1707232dd335ec01e058432b38
Sidney Engstrom <sidneyengstrom@gmail.com>
2024-01-23 19:26:18 -0800

    Adds date and layout resize functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,24 @@
 'use strict';
+const siteWrapper = document.querySelector(".site-wrapper");
+const copyright = document.querySelector(".copyright");
+
+
+function layoutResize() {
+    let screenWidth = window.innerWidth;
+
+    if (screenWidth >= 768) {
+        siteWrapper.classList.add("desktop");
+    } else if (screenWidth < 768) {
+        siteWrapper.classList.remove("desktop");
+    }
+}
+
+function updateCopyrightDate() {
+    let currentDate = new Date();
+    let currentYear = currentDate.getFullYear();
+    copyright.innerHTML = `&copy; Sidney Engstrom, ${currentYear}`;
+}
+
+window.addEventListener("resize", layoutResize);
+updateCopyrightDate();
+
